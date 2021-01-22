84e7c0694bc17daf8a1c1134098216d0fbabe6d6
Ben G <bensgar92@gmail.com>
2021-01-21 17:21:56 -0700

    initial commit, started front and back end of project

---
+++ mybackend/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 5000
+//if I were to send an http request to localhost:500/ then it would send back the string 'Hello World'
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/newEndPoint', (req, res) => {
+    res.send('Testing new endpoint!')
+})
+
+app.get('/test', (req, res) => {
+    if (2 + 2 === 4) {
+        res.send("2 + 2 does equal 4!");
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
